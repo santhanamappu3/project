44f7b99d1ac59ddf111328e133c7dc733cd0166f
santhanamappu3 <santhanamappu3@gmail.com>
2022-09-22 08:08:09 +0530

    re

---
+++ src/edu/dev/java/test/BaseClass.java
@@ -0,0 +1,26 @@
+package edu.dev.java.test;
+public class BaseClass {//baseclass concept
+	
+	private int add(int a, int b) {
+		int c = a+b;
+		return c;
+
+	}
+   public static void main(String[] args) {
+	BaseClass b = new BaseClass();
+	
+	System.out.println(b.add(10, 30));
+	System.out.println(b.add(20, 30));
+}
+
+}
+	
+	
+	
+	
+
+
+		
+		
+
+
